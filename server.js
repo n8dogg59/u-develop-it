549105dc7576d2df9b15c83f10ec74b0454b4edf
Nate Schroeder <n8dogg59@hotmail.com>
2021-03-08 18:40:22 -0600

    add post route

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const express = require('express');
+const inputCheck = require('./utils/inputCheck');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -77,6 +79,31 @@ app.delete('/api/candidate/:id', (req, res) => {
         message: 'successfully deleted',
         changes: this.changes
       });
+    });
+  });
+
+  // Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+              VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    // ES5 function, not arrow function, to use `this`
+    db.run(sql, params, function(err, result) {
+    if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+    }
+
+    res.json({
+        message: 'success',
+        data: body,
+        id: this.lastID
+    });
     });
   });
 
